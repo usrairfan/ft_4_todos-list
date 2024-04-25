145ddc291add181399f8b769ab7f48cdc7638911
usrairfan <160382971+usrairfan@users.noreply.github.com>
2024-04-25 14:55:27 +0500

    push my todos list to github

---
+++ index.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todos = []
+let condition = true;
+while(condition)
+    {
+let todosAnswers = await inquirer.prompt(
+
+    [
+      {
+        name: "firstQuestion",
+        type:"input",
+        message: "what whould you like to in your todos?"
+
+        },
+
+     {
+        name:"secondQuestion",
+        type:"confirm",
+        message:"what whould you like to add more in your todos",
+        defaoult:"true"
+      }
+
+        ]
+    
+
+);
+todos.push(todosAnswers.firstQuestion);
+console.log(todos);
+condition = todosAnswers.secondQuestion;
+}
